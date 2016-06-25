1162a26583f0947a8c8d039a8e34ce00bdeacaf6
Alexpri <Alexpri@github.com>
2016-06-25 18:59:49 +0300

    parse item url

--- app.js
+++ app.js
@@ -4,57 +4,78 @@ const qs = require('querystring');
 const http = require('http');
 const url = require('url');
 const got = require('got');
-const queue = [];
-//const indexPage = require(__dirname + '/public/tmpl');
+const fs = require('fs');
+//const htmlparser = require("htmlparser2");
+const cheerio = require('cheerio');
+var queue = [];
 
-//app.set('views', __dirname + '/template');
-//app.set('view engine', 'ejs');
+app.set('port', 3000)
+
+http.createServer(app).listen(app.get('port'));
 
 app.use(express.static(__dirname + '/public'));
 
 app.use(function (req, res) {
 	const query = qs.parse(url.parse(req.url).query);
 	const queryUrl = query["input-url"];
-	var contentUrl;
+	const queryUrlHostObj = qs.parse(url.parse(queryUrl).host);
+	var queryUrlHost;
+	for (key in queryUrlHostObj) {
+		queryUrlHost = key;
+	}
 
 	if (queryUrl) {
 		got(queryUrl)
 			.then(response => {
-				contentUrl = response.body;
-				//=> '<!doctype html> ...' 
-				res.end(contentUrl);
+				var contentUrl = response.body;
+				var $ = cheerio.load(contentUrl);
+
+				$('a').each(function (index, item) {
+					var itemUrlHostObj = qs.parse(url.parse(item.attribs.href).host),
+						itemUrlHost,
+						itemUrlInside;
+
+					for (key in itemUrlHostObj) {
+						itemUrlHost = key;
+					}
+
+
+					if (itemUrlHost == queryUrlHost) {
+						//homepage + внутренняя ссылки(сайт/внутрненняя) или ссылка на homepage
+						console.log(url.parse(item.attribs.href).host, url.parse(item.attribs.href).href);
+						//console.log(url.parse(item.attribs.href).host, itemUrlHost);
+						queue.push({
+							number: index,
+							text:  $(item).text().trim()
+						});
+					}
+
+					if (itemUrlHost == null) {
+						//только внутренняя ссылки (внутрненняя)
+						itemUrlInside = url.parse(item.attribs.href).path;
+
+						queue.push({
+							baseUrl: queryUrlHost,
+							insideUrl: itemUrlInside,
+							number: index,
+							text:  $(item).text().trim()
+						});
+					}
+				});
+
+				fs.writeFile('./tagsInfo.json', JSON.stringify(queue, null, 2), function (err) {
+					if(err) {
+				        return console.log(err);
+				    }
+				    console.log('file saved');
+					
+				});
+
+				res.end(JSON.stringify(queue, null, 2));
 			})
 			.catch(error => {
 				console.log(error.response.body);
 				//=> 'Internal server error ...' 
 			});
 	}
-	//res.render("index", function (req, res, next) {
-	//	page: '' + 	contentUrl;
-	//});
-	//res.end('yes');
 });
-
-
-/*app.use(function (req, res) {
-	if (req.url == '/vote') {
-	 	console.log('vote');
-	 	res.end('yes');
-	}
-});*/
-
-app.listen(3000);
-
-/*function accept(req, res) {
-
-  // если URL запроса /vote, то...
-  if (req.url == '/vote') {
-    // через 1.5 секунды ответить сообщением
-    setTimeout(function() {
-      res.end('Ваш голос принят: ' + new Date());
-    }, 1500);
-  } else {
-    // иначе считаем это запросом к обычному файлу и выводим его
-    file.serve(req, res); // (если он есть)
-  }
-}*/
